6efd0b611d50cbf8529c8b6ab44bc7fe07fa3c96
Simon Nguyen <simonnguyen99@gmail.com>
2024-03-12 12:39:41 -0700

    Added the basic functionality to Pizza.java

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,3 +1,22 @@
 public class Pizza {
 
+    private String[] toppings;
+    private String size;
+    private String pizzaChain;
+
+    public void eat() {
+        System.out.println("Pizza Chain: " + pizzaChain);
+        System.out.println("Size: " + size);
+        System.out.print("Toppings: ");
+        if(toppings.length == 0) {
+            System.out.println("n/a");
+        }
+        else {
+            for(String topping: toppings) {
+                System.out.println(topping + ", ");
+            }
+        }
+    }
+
+
 }
