05b16c9e838c4addd1ff64eb0b780d4b55208a37
Simon Nguyen <simonnguyen99@gmail.com>
2024-03-14 15:34:22 -0700

    Updated Driver

--- src/MacronutrientMeals/Driver.java
+++ src/MacronutrientMeals/Driver.java
@@ -5,6 +5,16 @@ public class Driver {
 
         Customer c1 = new Customer("Edward", "no restriction");
 
+        DietPlanFactory mealPlanner = DietPlanFactory.getInstance();
+
+        MacronutrientFactory c1Plan = mealPlanner.createPlan(c1.getDietPlan());
+        c1Plan.printPlan();
+
+        Customer c2 = new Customer("Cullen", "no restriction");
+        MacronutrientFactory c2Plan = mealPlanner.createPlan(c2.getDietPlan());
+        c2Plan.printPlan();
+
+
 
     }
 }
