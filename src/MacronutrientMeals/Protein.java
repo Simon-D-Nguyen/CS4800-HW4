35b8029a7be05366324edc2ffa0fedc97296d3f0
Simon Nguyen <simonnguyen99@gmail.com>
2024-03-15 20:36:05 -0700

    Updated Protein and ProteinFactory

--- src/MacronutrientMeals/Protein.java
+++ src/MacronutrientMeals/Protein.java
@@ -1,5 +1,16 @@
 package MacronutrientMeals;
 
-public interface Protein {
-    String[] mealProtein();
+public class Protein extends Macros{
+    public Protein() {
+        loadOptions();
+    }
+
+
+    @Override
+    protected void loadOptions() {
+        options.put(FoodCategory.meat, "Fish");
+        options.put(FoodCategory.meat, "Chicken");
+        options.put(FoodCategory.meat, "Beef");
+        options.put(FoodCategory.veg, "Tofu");
+    }
 }
--- src/MacronutrientMeals/ProteinFactory.java
+++ src/MacronutrientMeals/ProteinFactory.java
@@ -1,4 +1,42 @@
 package MacronutrientMeals;
 
 public class ProteinFactory {
+    public static ProteinFactory factory = null;
+    private ProteinFactory(){}
+
+
+    public static ProteinFactory getInstance(){
+        if(factory == null) {
+            factory = new ProteinFactory();
+        }
+        return factory;
+    }
+
+
+    public Protein getProtein(DietPlan dietPlan) {
+        switch (dietPlan) {
+            case DietPlan.NoRestriction:
+                return new Protein();
+
+            case DietPlan.Paleo:
+                Protein paleo = new Protein();
+                paleo.removeDairy();
+                paleo.removeVeg();
+                return paleo;
+
+            case DietPlan.Vegan:
+                Protein vegan = new Protein();
+                vegan.removeDairy();
+                vegan.removeMeats();
+                return vegan;
+
+            case DietPlan.NutAllergy:
+                Protein noNuts = new Protein();
+                noNuts.removeNuts();
+                return noNuts;
+
+            default:
+                return null;
+        }
+    }
 }
